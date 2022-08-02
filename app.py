799fe41b6ca6eb5a7754dd90c3f07df81e2e0ae3
petal416 <petal416@naver.com>
2022-08-02 12:18:05 +0900

    1

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-print("Hello World")
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from pymongo import MongoClient
+import certifi
 
-asdfsd
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.72mx2.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
