9147373106c5ad2d7ca9634aa3181705b73fe6fb
ddoobbb <dk3hek@gmail.com>
2022-08-02 09:29:37 +0900

    dd

--- test/first.py
+++ test/first.py
@@ -1,2 +1,13 @@
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template('index.html', nickname=user_info["nick"])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 JK = '첫 테스트!!!'
 print(JK)
