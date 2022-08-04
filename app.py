39c5b87aa1e19d986530efa1cb5aa50c891daaa1
JinKyong <sjk900700@gmail.com>
2022-08-04 14:50:58 +0900

    코드 전반적으로 보면서 수정사항 및 필요없는 부분 삭제

--- app.py
+++ app.py
@@ -114,21 +114,21 @@ def save_img():
         username = payload["id"]
         name_receive = request.form["name_give"]
         about_receive = request.form["about_give"]
-        password_receive = request.form["password_give"]
-        pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
         new_doc = {
             "nickname": name_receive,
-            "profile_info": about_receive,
-            "password": pw_hash
+            "profile_info": about_receive
         }
+        # 파일 이름 원하는 형태로 가공
         if 'file_give' in request.files:
             file = request.files["file_give"]
             filename = secure_filename(file.filename)
             extension = filename.split(".")[-1]
             file_path = f"profile_pics/{username}.{extension}"
             file.save("./static/" + file_path)
+            # doc에 추가
             new_doc["profile_pic"] = filename
             new_doc["profile_pic_real"] = file_path
+
         db.users.update_one({'username': payload['id']}, {'$set': new_doc})
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
@@ -161,8 +161,6 @@ def detailInfo(address):
     except jwt.exceptions.DecodeError:
         return render_template("detail.html", address=address, data=json.dumps(data),
                                user_info=None, msg=None)
-
-    return
 
 
 # 프로필 페이지 서버
@@ -213,7 +211,7 @@ def get_reviews_user():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        my_username = payload["id"]
+        # my_username = payload["id"]
         username_receive = request.args.get("username_give")
 
         if username_receive == "":
